d1af792882343d80668a8bde7c80409d09383eed
Hajo Haas <hajo.haas@hmmh.de>
2025-09-21 18:51:03 +0200

    add keybinds for localleader

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.g.mapleader = " "
+local bs = "<BS>" -- secondary leader
 
 local map = vim.keymap.set
 -- local del = vim.keymap.del
@@ -22,9 +23,6 @@ map("v", "d", '"_d')
 map("v", "c", '"_c')
 map("n", "<S-BS>", '"_dd', { noremap = true, silent = true })
 map("x", "<leader>p", '"_dP', { noremap = true, silent = true })
-
--- backspace to switch to previous buffer
-map("n", "<bs>", "<C-^>")
 
 -- center on next selection
 map("n", "n", "nzz", { noremap = true, silent = true })
@@ -59,4 +57,60 @@ map("n", "<leader>cS", function()
     mode = "lsp",
     focus = true,
   })
-end, { desc = "LSP refs/defs/type (Trouble, no focus)" })
+end, { desc = "LSP refs/defs/type" })
+
+-- Navigation
+vim.keymap.set("n", bs .. "s", "<cmd>AerialNavToggle<cr>", { desc = "Aerial Nav" })
+vim.keymap.set("n", bs .. "S", function()
+  require("trouble").toggle({
+    source = "lsp",
+    mode = "lsp",
+    focus = true,
+  })
+end, { desc = "Trouble LSP (refs/defs/types)" })
+vim.keymap.set("n", bs .. "r", "<cmd>FzfLua live_grep_resume<cr>", { desc = "Resume Grep" })
+
+-- Diagnostics
+local diagnostic_goto = function(next, severity)
+  local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
+  severity = severity and vim.diagnostic.severity[severity] or nil
+  return function()
+    go({ severity = severity })
+  end
+end
+vim.keymap.set("n", bs .. "d", diagnostic_goto(true), { desc = "Next Diagnostic" })
+vim.keymap.set("n", bs .. "D", function()
+  require("trouble").toggle({
+    source = "diagnostics",
+    mode = "diagnostics",
+    focus = true,
+  })
+end, { desc = "Trouble Diagnostics" })
+
+-- Quickfix
+vim.keymap.set("n", bs .. "q", "<cmd>copen<cr>", { desc = "Quickfix toggle" })
+vim.keymap.set("n", bs .. "n", "<cmd>cnext<cr>", { desc = "Quickfix Next" })
+vim.keymap.set("n", bs .. "p", "<cmd>cprev<cr>", { desc = "Quickfix Prev" })
+
+-- LSP Goodies
+vim.keymap.set("n", bs .. "c", vim.lsp.buf.code_action, { desc = "Code Action" })
+
+-- Extras
+vim.keymap.set("n", bs .. "t", "<cmd>Trouble todo toggle<cr>", { desc = "Todo Comments" })
+vim.keymap.set("n", bs .. "o", "<cmd>only<cr>", { desc = "Only window" })
+
+-- UndoTree
+vim.keymap.set("n", bs .. "u", function()
+  require("undotree").toggle()
+end, { desc = "Undo Tree" })
+
+-- Debug / Logging
+vim.keymap.set("n", bs .. "l", function()
+  require("refactoring").debug.printf({ below = false })
+end, { desc = "Debug Print" })
+vim.keymap.set({ "n", "x" }, bs .. "L", function()
+  require("refactoring").debug.print_var({ normal = true })
+end, { desc = "Debug Print Variable" })
+vim.keymap.set("n", bs .. "x", function()
+  require("refactoring").debug.cleanup({})
+end, { desc = "Debug Cleanup" })
