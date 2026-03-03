8357fd92edc7ffa262708e87cdf476978599023e
Hajo Haas <hajo.haas@hmmh.de>
2026-03-03 11:37:13 +0100

    chore: remove diffview keymaps

--- common/.config/nvim/lua/plugins/diffview.lua
+++ common/.config/nvim/lua/plugins/diffview.lua
@@ -10,10 +10,6 @@ return {
       "DiffviewFocusFiles",
       "DiffviewRefresh",
     },
-    keys = {
-      { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview" },
-      { "<leader>gD", "<cmd>DiffviewOpen origin/develop...HEAD<cr>", desc = "Diffview vs develop" },
-      { "<leader>gH", "<cmd>DiffviewFileHistory %<cr>", desc = "Diffview file history" },
-    },
+
   },
 }
