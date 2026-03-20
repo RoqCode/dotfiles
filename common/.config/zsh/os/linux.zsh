53329d4ed7bae5d9f91b572e316035e04e51b698
Hajo Haas <hajo.haas@hmmh.de>
2026-03-20 17:08:29 +0100

    feat: install pnpm on linux

--- common/.config/zsh/os/linux.zsh
+++ common/.config/zsh/os/linux.zsh
@@ -1,1 +1,9 @@
 export _ZO_DATA_DIR="${XDG_DATA_HOME:-$HOME/.local/share}"
+
+# pnpm
+export PNPM_HOME="$HOME/.local/share/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
